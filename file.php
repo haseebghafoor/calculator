80011af0f82a1441a24a7f5b1585be2b7c3d9889
LAPTOP-H3QJ723G\M.Haseeb Ghafoor <haseeb.ghafoor@gmail.com>
2018-01-09 15:16:34 +0500

    calculator front end code

---
+++ file.php
@@ -0,0 +1,48 @@
+
+<html>
+<head>
+<body>
+<table>
+<form method="post" action="data.php">
+<tr>
+
+<td>
+First number
+</td>
+<td>
+<input type="text" name="fname">
+</td>
+</tr>
+<tr>
+
+<td>
+Second Number
+</td>
+<td>
+<input type="text" name="lname" >
+</td>
+</tr>
+<tr>
+
+
+<td>
+<input type="submit" name="add" value="+" >
+</td>
+<td>
+<input type="submit" name="sub" value="-">
+</td>
+
+<td>
+<input type="submit" name="mul" value="*">
+</td>
+<td>
+<input type="submit" name="div" value="/">
+</td>
+</tr>
+</form>
+</table>
+
+</body>
+
+</head>
+</html>
